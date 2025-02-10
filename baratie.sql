bfd3fd9e1b13c334eb80c46de58f6a4fe6e01e6e
Ingryd <ingrydcduarte@gmail.com>
2025-02-10 10:45:56 -0300

    up: banco sql

--- baratie.sql
+++ baratie.sql
@@ -16,11 +16,20 @@ senha varchar (12) not null);
 CREATE TABLE tbpratos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
-    descricao VARCHAR(25) NULL,
+    descricao VARCHAR(50) NULL,
     quant INT NOT NULL,
-    preco INT NOT NULL,
-    tipo VARCHAR (25) NOT NULL;
+    preco FLOAT NOT NULL,
+    categoria VARCHAR (25) NOT NULL
 );
+
+CREATE TABLE tbacomp (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    descricao VARCHAR(50) NULL,
+    quant INT NOT NULL,
+    preco FLOAT NOT NULL,
+    categoria VARCHAR (25) NOT NULL
+)
 
 select * from tbclientes;
 
@@ -29,3 +38,8 @@ VALUES ('Ingryd', 'mail@gmail.com', 'senha1234');
 
 INSERT INTO tbadmins (nome, login, senha) 
 VALUES ('JP', 'adm@gmail.com', 'admin@123');
+
+INSERT INTO tbacomp (nome, descricao, quant, preco, categoria) 
+VALUES ('Salada Verde', 'Alface, tomate e cebola, com adição de azeite', '250', '10.00', 'saladas');
+
+drop table tbpratos;
